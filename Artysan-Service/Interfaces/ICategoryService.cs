dfaeaa3c89434cffdcee3220b68a7ac8c6979ed5
altaysenturk82@gmail.com <altaysenturk82@gmail.com>
2024-08-10 21:25:18 +0300

    mapping eklendi (referans hatası var ona bakılacak)

--- Artysan-Service/Interfaces/ICategoryService.cs
+++ Artysan-Service/Interfaces/ICategoryService.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Artysan_Service.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Artysan_Service.Interfaces
 {
 	public interface ICategoryService
 	{
+		Task<IEnumerable<CategoryViewModel>> GetAll();
 	}
 }
---
+++ Artysan-Service/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Artysan_Service.ViewModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artysan_Service.Mapping
+{
+	public class MappingProfile : Profile
+	{
+        public MappingProfile()
+        {
+            //CreateMap<Category, CategoryViewModel>().ReverseMap();
+        }
+    }
+}
