be9905a94a9d878ab7c258383612c18b4aa711dc
altaysenturk82@gmail.com <altaysenturk82@gmail.com>
2024-08-19 18:40:23 +0300

    images migrationu alındı

--- Artysan-DAL/Migrations/20240819154000_Images.Designer.cs
+++ Artysan-DAL/Migrations/20240819154000_Images.Designer.cs
@@ -4,6 +4,7 @@ using Artysan_DAL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Artysan_DAL.Migrations
 {
     [DbContext(typeof(ArtysanDbContext))]
-    partial class ArtysanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240819154000_Images")]
+    partial class Images
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
