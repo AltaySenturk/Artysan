ca79b2e82dd4f1f395ad069f572ccff763df935e
altaysenturk82@gmail.com <altaysenturk82@gmail.com>
2024-08-26 13:52:58 +0300

    ufak değişiklikler

--- Artysan-App/Areas/Admin/Controllers/AdminEventController.cs
+++ Artysan-App/Areas/Admin/Controllers/AdminEventController.cs
@@ -6,38 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Artysan_App.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize]
-    public class AdminEventController : Controller
-    {
-        private readonly IEventService _eventService;
+	[Area("Admin")]
+	[Authorize]
+	public class AdminEventController : Controller
+	{
+		private readonly IEventService _eventService;
 
-        public AdminEventController(IEventService eventService)
-        {
-            _eventService = eventService;
-        }
+		public AdminEventController(IEventService eventService)
+		{
+			_eventService = eventService;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            var events = await _eventService.GetAll();
-            return View(events);
-        }
-        public IActionResult Add()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			var events = await _eventService.GetAll();
+			return View(events);
+		}
+		public IActionResult Add()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Add(EventViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-               ModelState.AddModelError("", "Kullanıcı kaydı gerçekleşmedi!");
-                return View(model);
-          }
-              
-              _eventService.Add(model);
-             return RedirectToAction("Index");
-        }
-    }
+		[HttpPost]
+		public IActionResult Add(EventViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				ModelState.AddModelError("", "Kullanıcı kaydı gerçekleşmedi!");
+				return View(model);
+			}
+
+			_eventService.Add(model);
+			return RedirectToAction("Index");
+		}
+	}
 }
